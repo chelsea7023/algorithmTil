a0962f46d6fc29dfe982cfcb8651b5a21860c758
chelsea7023 <yunhongkim123@gmail.com>
2024-03-27 01:19:49 +0900

    algorithm

---
+++ 24-03-26/src/BOJ_2810_컵홀더/Main.java
@@ -0,0 +1,37 @@
+package BOJ_2810_컵홀더;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new  Scanner(System.in);
+		
+		int N = sc.nextInt(); // 자리수 
+		String str = sc.next();
+		char[] seet = str.toCharArray();
+		int cnt = 0; // 컵홀더 수 
+		int d = 1;
+		if(seet[0] == 'S') {
+			cnt+=2;
+		} else if(seet[0] == 'L') {
+			cnt+=2;
+			d++;
+		}
+		
+		while(d < seet.length) {
+			if(seet[d] == 'S') {
+				cnt += 1;
+				d++;
+			}else if(seet[d] == 'L') {
+				cnt += 1;
+				d += 2;
+			}
+		}
+		if(cnt > N) {
+			cnt = N;
+		}
+		
+		
+		System.out.println(cnt);
+	}
+}
