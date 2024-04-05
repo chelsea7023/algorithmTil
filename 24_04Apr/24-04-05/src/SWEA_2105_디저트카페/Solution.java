3e1c25fbcc350e48621a8823c03531e2c84e4976
james7023 <yunhongkim123@gamil.com>
2024-04-05 17:51:41 +0900

    algorithm

--- 24_04Apr/24-04-05/src/SWEA_2105_디저트카페/Solution.java
+++ 24_04Apr/24-04-05/src/SWEA_2105_디저트카페/Solution.java
@@ -3,6 +3,8 @@ package SWEA_2105_디저트카페;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -12,6 +14,9 @@ public class Solution {
 	static int[] dj = { 1, -1, -1, 1 };
 	static int[][] map;
 	static boolean flag = false;
+	static List<Integer> list = new ArrayList<>();
+	static boolean[][] visited;
+	static List<Integer> check = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,6 +27,7 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
+			visited = new boolean[N][N];
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -29,7 +35,32 @@ public class Solution {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					check = new ArrayList<>();
+					dfs(i, j, i, j, 1);
+				}
+			}
+			
+			System.out.println(list);
 
+		}
+	}
+
+	static void dfs(int x, int y, int r, int c, int num) {
+		
+		if(visited[x][y] && x == r && y == c) {
+			list.add(num);
+		}
+		
+		for (int d = 0; d < 4; d++) {
+			int nx = x + di[d];
+			int ny = y + dj[d];
+			if (check(nx, ny) && !visited[nx][ny] && !check.contains(map[nx][ny])) {
+				visited[nx][ny] = true;
+				dfs(nx, ny, r, c, num + 1);
+			}
 		}
 	}
 
