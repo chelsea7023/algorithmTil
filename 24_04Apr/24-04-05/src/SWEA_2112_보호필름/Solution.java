4ccd22adaaa0f33a56f2c1e615902c36b5826abc
james7023 <yunhongkim123@gamil.com>
2024-04-05 13:29:59 +0900

    algorithm

--- 24_04Apr/24-04-05/src/SWEA_2112_보호필름/Solution.java
+++ 24_04Apr/24-04-05/src/SWEA_2112_보호필름/Solution.java
@@ -15,15 +15,22 @@ public class Solution {
 	static int[][] copy;
 	static int[][] map;
 	static int D, W, K, answer;
+	static List<int[]> combination;
+	static boolean flag;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int T = Integer.parseInt(st.nextToken());
 
 		for (int tc = 1; tc <= T; tc++) {
+
+			flag = true;
+
 			st = new StringTokenizer(br.readLine());
+
 			D = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
@@ -38,16 +45,23 @@ public class Solution {
 				}
 			}
 
-			copy = map;
+			check();
 
-			boolean flag = check();
+			copy = new int[D][W];
+
+			for (int i = 0; i < D; i++) {
+				copy[i] = Arrays.copyOf(map[i], W);
+			}
 
 			int[] arr = new int[D];
+
 			for (int i = 0; i < D; i++) {
 				arr[i] = i;
 			}
+
 			List<Integer> list = new ArrayList<>();
 			List<List<Integer>> result = new ArrayList<>();
+
 			for (int i = 0; i < (1 << D); i++) {
 				list = new ArrayList<>();
 				for (int j = 0; j < D; j++) {
@@ -62,40 +76,76 @@ public class Solution {
 
 			Collections.sort(result, Comparator.comparingInt(List::size));
 
-			int[] nums = new int[] { 0, 1 };
+			combination = new ArrayList<>();
 
-			OUT: while (!flag) {
-				for (int i = 0; i < result.size(); i++) {
-
-					for (int j = 0; j < result.get(i).size(); j++) {
-						int num = result.get(i).get(j);
-						Arrays.fill(map[num], nums[0]);
-					}
-
-					answer = result.get(i).size();
-					flag = check();
-					if (flag) {
-						break OUT;
-					}
-					map = copy;
-
-				}
-
+			for (int i = 1; i <= K; i++) {
+				generateCombination(i);
 			}
-			if (flag) {
-				System.out.println("#" + tc + " " + 0);
-			} else {
+
+			OUT:
+
+			while (!flag) {
+
+				IT: for (int i = 0; i < result.size(); i++) {
+
+					for (int k = 0; k < combination.size(); k++) {
+
+						if (result.get(i).size() == combination.get(k).length) {
+
+							for (int l = 0; l < result.get(i).size(); l++) {
+
+								Arrays.fill(map[result.get(i).get(l)], combination.get(k)[l]);
+
+							}
+						}else if(result.get(i).size() < combination.get(k).length){
+							continue IT;
+						}
+
+						answer = result.get(i).size();
+
+						check();
+
+						if (flag) {
+							flag = false;
+							break OUT;
+						}
+
+						for (int n = 0; n < D; n++) {
+							map[n] = Arrays.copyOf(copy[n], W);
+						}
+
+					}
+				}
+			}
+
+			if (!flag) {
 				System.out.println("#" + tc + " " + answer);
+			} else {
+				System.out.println("#" + tc + " " + 0);
 			}
 
 		}
 
 	}
 
-	static boolean check() {
-		int pass = 0;
+	static void generateCombination(int x) {
+		for (int i = 0; i < Math.pow(2, x); i++) {
+			int[] com = new int[x];
+			int temp = i;
+			for (int j = x - 1; j >= 0; j--) {
+				com[j] = temp % 2;
+				temp /= 2;
+			}
+			combination.add(com);
+		}
+	}
+
+	static void check() {
+
 		This: for (int j = 0; j < W; j++) {
-			for (int i = 0; i < D - K; i++) {
+			int pass = 0;
+
+			for (int i = 0; i <= D - K; i++) {
 				int num = map[i][j];
 				int cnt = 0;
 				for (int k = 0; k < K; k++) {
@@ -103,19 +153,21 @@ public class Solution {
 						cnt++;
 					}
 				}
-				if (cnt >= K - 1) {
+				if (cnt >= K) {
 					pass++;
-					continue This;
 				}
 
 			}
 
-		}
-		if (pass >= W) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+			if (pass < 1) {
+				flag = false;
+				return;
+			} else if (pass >= 1) {
+				continue This;
+			}
 
+		}
+		flag = true;
+		return;
+	}
 }
