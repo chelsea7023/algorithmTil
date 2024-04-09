277a28b32d67c2a307b466cab30c778341d8ac8b
chelsea7023 <yunhongkim123@gmail.com>
2024-04-09 22:52:27 +0900

    algorithm

--- UNSOLVED/src/SWEA_2105_디저트카페/Solution.java
+++ UNSOLVED/src/SWEA_2105_디저트카페/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,10 +14,7 @@ public class Solution {
 	static int[] di = { 1, 1, -1, -1 };
 	static int[] dj = { 1, -1, -1, 1 };
 	static int[][] map;
-	static boolean flag = false;
-	static List<Integer> list = new ArrayList<>();
-	static boolean[][] visited;
-	static List<Integer> check;
+	static List<Integer> list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,7 +25,7 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
-			visited = new boolean[N][N];
+			list = new ArrayList<>();
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -40,41 +38,48 @@ public class Solution {
 
 				for (int j = 0; j < N; j++) {
 
-					check = new ArrayList<>();
-					visited = new boolean[N][N];
-					check.add(map[i][j]);
+					boolean[][] visited = new boolean[N][N];
+
+					List<Integer> check = new ArrayList<>();
+
 					visited[i][j] = true;
-					dfs(i, j, i, j, 1);
+					check.add(map[i][j]);
+
+					dfs(map, visited, check, i, j, i, j, 1);
 
 				}
 			}
 
-			System.out.println(list);
-
+			if (!list.isEmpty()) {
+				Collections.sort(list, Collections.reverseOrder());
+				System.out.print("#" + tc + " " + list.get(0));
+			} else {
+				System.out.println(-1);
+			}
 		}
 	}
 
-	static void dfs(int x, int y, int r, int c, int dp) {
-
-		if (x == r && y == c && visited[x][y] && dp > 2) {
+	static void dfs(int[][] arr, boolean[][] visited, List<Integer> num, int x, int y, int r, int c, int dp) {
+		if (x == r && y == c && visited[x][y] && dp >= 4) {
 			list.add(dp);
 			return;
 		}
 
 		for (int d = 0; d < 4; d++) {
-
 			int nx = x + di[d];
 			int ny = y + dj[d];
 
-			if (check(nx, ny) && !visited[nx][ny] && !check.contains(map[nx][ny])) {
+			if (check(nx, ny) && !visited[nx][ny] && !num.contains(map[nx][ny])) {
 				visited[nx][ny] = true;
-				dfs(nx, ny, r, c, dp + 1);
-
+				num.add(map[nx][ny]);
+				dfs(arr, visited, num, nx, ny, r, c, dp + 1);
+//				visited[nx][ny] = false;
+				num.remove(Integer.valueOf(map[nx][ny]));
 			} else if (nx == r && ny == c && dp > 2) {
-				dfs(nx, ny, r, c, dp + 1);
+				dfs(arr, visited, num, nx, ny, r, c, dp);
 			}
-		}
 
+		}
 	}
 
 	static boolean check(int x, int y) {
