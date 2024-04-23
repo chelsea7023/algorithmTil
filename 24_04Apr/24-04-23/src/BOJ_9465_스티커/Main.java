688e2d27345d41b4e132bbee6cb72aaf52cf3147
chelsea7023 <yunhongkim123@gmail.com>
2024-04-23 17:59:38 +0900

    algorithm

---
+++ 24_04Apr/24-04-23/src/BOJ_9465_스티커/Main.java
@@ -0,0 +1,28 @@
+package BOJ_9465_스티커;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 0; tc < T; tc++) {
+			int N = sc.nextInt();
+
+			int[][] card = new int[2][N];
+
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < N; j++) {
+					card[i][j] = sc.nextInt();
+				}
+			}
+
+			
+			
+			
+
+		}
+	}
+}
