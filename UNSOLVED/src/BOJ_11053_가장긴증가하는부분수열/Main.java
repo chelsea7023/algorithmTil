93a311477eb1587c51cc9dc713d1f0c586567612
chelsea7023 <yunhongkim123@gmail.com>
2024-05-02 09:00:41 +0900

    algorithm

---
+++ UNSOLVED/src/BOJ_11053_가장긴증가하는부분수열/Main.java
@@ -0,0 +1,18 @@
+package BOJ_11053_가장긴증가하는부분수열;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		
+		
+	}
+}
