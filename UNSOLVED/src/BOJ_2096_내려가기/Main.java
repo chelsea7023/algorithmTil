3d3f7ea4f3f6c3207c6706e762a0518f9d9d1250
chelsea7023 <yunhongkim123@gmail.com>
2024-05-11 11:38:29 +0900

    ing

---
+++ UNSOLVED/src/BOJ_2096_내려가기/Main.java
@@ -0,0 +1,14 @@
+package BOJ_2096_내려가기;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[][] dp = new int[N + 1][N + 1];
+		
+		
+	}
+}
