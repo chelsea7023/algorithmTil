23846fc50ddc429ec831842d09a83cf286e0fad7
chelsea7023 <156149302+chelsea7023@users.noreply.github.com>
2024-08-15 02:14:10 +0900

    [Bronze V] Title: 문자와 문자열, Time: 164 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] arr = str.toCharArray();
+
+        System.out.println(arr[sc.nextInt() - 1]);
+
+    }
+}
